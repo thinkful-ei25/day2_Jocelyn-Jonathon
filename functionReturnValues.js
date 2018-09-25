66750cee173c5cee7927e6a4205e523f4d356414
Reldalk <Reldalk@gmail.com>
2018-09-25 14:34:07 -0400

    Function REturn values

---
+++ functionReturnValues.js
@@ -0,0 +1,17 @@
+function hazardWarningCreator(typeOfWarning){
+    let warningCounter = 0;
+    return function(location){
+        warningCounter++;
+        console.log(`DANGER! There is a ${typeOfWarning} hazard at ${location}!`);
+        console.log(`The ${typeOfWarning} hazard alert has triggered ${warningCounter} time(s) today!`);
+
+    };
+}
+alertArray = ["Rocks on the Road", "Balloons in the air", "People on the ground"];
+locationArray = ["Florida", "California", "Ohio"]; 
+for(let i = 0; i < 3; i++){
+    let warning = hazardWarningCreator(alertArray[i]);
+    console.log(warning(locationArray[i]));
+    console.log(warning(locationArray[i]));
+    console.log(warning(locationArray[i]));
+}
